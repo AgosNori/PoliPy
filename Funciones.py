171b87e9af441c02a3e14f2956b84f03ddc3b001
agos <agosnoriega2018@gmail.com>
2024-05-13 20:19:30 -0300

    agregando el archivo de func

---
+++ Funciones.py
@@ -0,0 +1,66 @@
+# Funciones
+# Retorna el valor absoluto del parametro que se pase
+a = -12
+print('El resultado de abs es: ',abs(a))
+
+#Obtiene la conversión a binario
+b = 0
+print('El resultado de bin es: ',bin(b))
+
+# Retornar el caracter en formato unicode
+c = 104
+print('El resultado de chr es: ',chr(c))
+
+# Retornar el cociente y el resto de una división
+d,e = 14,7
+cociente,resto= divmod(d,e)
+print('El cociente de la división es: ', cociente ,' y el resto',resto)
+
+# Convierte una cadena de texto a decimal
+f = '23.54'
+print('El resultado de la función float es: ',float(f))
+
+# Obtiene la conversión a hexadecimal
+g = 124
+print('El resultado de la función hex es: ',hex(g))
+
+# Le pide al usuario que ingrese una string (cadena de texto)
+h = input('Ingrese su nombre:' )
+print('El resultado de la función input es: ',h)
+
+# Convierte una cadena de texto a un numero entero
+i = '123'
+print('El resultado de la función int es: ', int(i))
+
+# Retorna la longitud del objeto tomado por parametro
+j = 12,23,45,13,1231
+print('El resultado de la función len es: ', len(j))
+
+# Retorna el mayor de los parametros tomados
+k,l = 12,43
+print('El resultado de la función max es: ',max(k,l))
+
+# Retorna el menor de los parametros tomados
+m,n = 12,43
+print('El resultado de la función min es: ',min(m,n))
+
+# Obtiene la conversión a octal
+o = 124
+print('El resultado de la función oct es: ',oct(o))
+
+# Retorna el entero unicode
+p = '<'
+print('El resultado de la función ord es: ',ord(p))
+
+# Retorna la potencia entre dos numeros
+q,r = 2,3
+print('El resultado de la función pow es: ', pow(q,r))
+
+# Redondea a la cantidad de decimales que quieran 
+s = 123.532245554
+print('El resultado de la función round es: ', round(s,2))
+
+# Convierte a cadena de texto
+t = 2,4,1
+print('El resultado de la función str es :',str(t))
+
