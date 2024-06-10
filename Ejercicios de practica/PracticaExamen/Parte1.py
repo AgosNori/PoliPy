29b82e5ae82b2f2e4b2ec0cd841de94904cee85a
agos <agosnoriega2018@gmail.com>
2024-06-10 19:58:44 -0300

    nuevos ejercicios agregados

--- Ejercicios de practica/PracticaExamen/Parte1.py
+++ Ejercicios de practica/PracticaExamen/Parte1.py
@@ -15,13 +15,55 @@
 # Escribe un programa que determine si un numero
 # entero es par o impar
 
-
-
-
 # Ejercicio 5:
-# Diseña un programa que indique si una persona puede obtener su licencia de conducir. Debe ser mayor de edad y pasar el examen teorico y practico.
-
-
-
+# Diseña un programa que indique si una persona 
+# puede obtener su licencia de conducir. Debe
+# ser mayor de edad y pasar el examen teorico
+# y practico.
 
 # Ejercicio 6:
+# Crea un programa que determine si un triangulo
+# es equilatero, isosceles o escaleno segun sus
+# lados
+
+
+# Ejercicio 7:
+# Escribe un programa que determine si una letra 
+# ingresada es vocal o consonante
+
+# Ejercicio 8: Diseña un programa que determine si 
+# un año es siglo xxi o no. Un año pertenece al 
+# sigo xxi si es mayor o igual que 2001 y/ o menor
+# o igual que 2100
+
+# Ejercicio 9: Crea un programa que clasifique
+# un triangulo segun sus angulos en agudo,
+# rectangulo u obtuso
+
+# Ejercicio 10:
+# Escribe un programa que determine si un estudiante
+# aprobo o no una materia, basandose en las tres 
+# calificaciones. Debe tener un promedio mayor a 7
+
+# Ejercicio 11:
+# Intercambiar los valores de una tupla de dos elementos
+
+# Ejercicio 12:
+# Calcula la suma de todos los elementos en una tupla de numeros
+
+# Ejercicio 13:
+# Encuentra el valor maximo y el valor minimo de una tupla de numeros
+
+# Ejercicio 14:
+# Toma 2 tuplas y concatenalas
+
+# Ejercicio 15:
+# Verifica que una tupla esta vacio o no. informar
+
+# Ejercicio 16:
+# Devuelve una nueva tupla con solo los elementos pares de la tupla original
+
+# Ejercicio 17:
+# Toma una tupla de cadenas y devuelve una nueva tupla con la longitud de cada cadena
+
+
