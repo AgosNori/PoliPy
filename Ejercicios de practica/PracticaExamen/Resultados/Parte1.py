2e7c1f0de2338d7fc686082926872bcc967a44dd
agos <agosnoriega2018@gmail.com>
2024-06-10 20:06:02 -0300

    ejercicios resueltos

---
+++ Ejercicios de practica/PracticaExamen/Resultados/Parte1.py
@@ -0,0 +1,13 @@
+# Resultados parte 1
+num = int(input('Ingrese un numero: '))
+if num > 0:
+    print('Es positivo')
+elif num == 0:
+    print('Es cero')
+else:
+    print('Es negativo')
+    
+# Resultado 2
+
+    
+    
