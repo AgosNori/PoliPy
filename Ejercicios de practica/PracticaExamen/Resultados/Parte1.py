c44b978186935f69b48613ff0f554f029b124ece
agos <agosnoriega2018@gmail.com>
2024-06-10 20:20:58 -0300

    primero 5 ejercicios resueltos

--- Ejercicios de practica/PracticaExamen/Resultados/Parte1.py
+++ Ejercicios de practica/PracticaExamen/Resultados/Parte1.py
@@ -8,6 +8,21 @@ else:
     print('Es negativo')
     
 # Resultado 2
+num1= int(input('Ingrese el primer numero: '))
+num2= int(input('Ingrese el segundo numero: '))
+    
+print('El mayor es: ',max(num1,num2))
 
+# Resultado 3
+año = int(input('Ingrese un año: '))
+if (año % 4 ==0 and año %100 != 0 or año %400==0):
+    print('año bisiesto')
+else:
+    print('año no bisiesto')
     
-    
+# Resultado 4
+numero = int(input('Ingrese un numero :'))
+if numero %2 == 0:
+    print('Es par')
+else:
+    print('No es par')
