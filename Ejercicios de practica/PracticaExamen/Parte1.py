d432f248524b12f422f35c5ff21339ca27596d33
agos <agosnoriega2018@gmail.com>
2024-06-10 19:36:11 -0300

    subiendo ejercicios de practica

---
+++ Ejercicios de practica/PracticaExamen/Parte1.py
@@ -0,0 +1,27 @@
+# Ejercicio 1:
+# Escribe un programa que pida al usuario un 
+# numero e imprima si es positivo, negativo o
+# cero
+
+# Ejercicio 2:
+# Diseña un programa que pida al usuario dos 
+# numeros y muestre el mayor de ellos
+
+# Ejercicio 3:
+# Crea un programa que determine si un año es
+# bisiesto o no. 
+
+# Ejercicio 4:
+# Escribe un programa que determine si un numero
+# entero es par o impar
+
+
+
+
+# Ejercicio 5:
+# Diseña un programa que indique si una persona puede obtener su licencia de conducir. Debe ser mayor de edad y pasar el examen teorico y practico.
+
+
+
+
+# Ejercicio 6:
--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-t=(1,2,3,4,5)
-for n in t:
-    m = max(t)
-print(m)
+
+ciudad= 'Rio Grande'
+for caracter in ciudad:
+    print(caracter)
