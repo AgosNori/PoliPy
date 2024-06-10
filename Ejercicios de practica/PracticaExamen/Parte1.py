866345f5d33859b295fb46ff824ec26058c902f0
agos <agosnoriega2018@gmail.com>
2024-06-10 20:46:51 -0300

    nuevos resultados

--- Ejercicios de practica/PracticaExamen/Parte1.py
+++ Ejercicios de practica/PracticaExamen/Parte1.py
@@ -49,10 +49,12 @@
 # Intercambiar los valores de una tupla de dos elementos
 
 # Ejercicio 12:
-# Calcula la suma de todos los elementos en una tupla de numeros
+# Calcula la suma de todos los elementos en una tupla de
+# numeros
 
 # Ejercicio 13:
-# Encuentra el valor maximo y el valor minimo de una tupla de numeros
+# Encuentra el valor maximo y el valor minimo de una tupla
+# de numeros
 
 # Ejercicio 14:
 # Toma 2 tuplas y concatenalas
@@ -61,9 +63,11 @@
 # Verifica que una tupla esta vacio o no. informar
 
 # Ejercicio 16:
-# Devuelve una nueva tupla con solo los elementos pares de la tupla original
+# Devuelve una nueva tupla con solo los elementos pares
+# de la tupla original
 
 # Ejercicio 17:
-# Toma una tupla de cadenas y devuelve una nueva tupla con la longitud de cada cadena
+# Toma una tupla de cadenas y devuelve una nueva tupla
+# con la longitud de cada cadena
 
 
