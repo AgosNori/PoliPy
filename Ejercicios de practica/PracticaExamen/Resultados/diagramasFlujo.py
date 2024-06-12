905f26b6e9c511641299e9268c13c4d26e47678a
agos <agosnoriega2018@gmail.com>
2024-06-12 20:18:04 -0300

    subiendo ejercicios para hacer los diagrama de flujos

---
+++ Ejercicios de practica/PracticaExamen/Resultados/diagramasFlujo.py
@@ -0,0 +1,33 @@
+# Diagrama de flujos
+
+# 1- Escribe un programa que solicite al usuario un número
+# y determine si es par o impar.
+
+# 2- Escribe un programa que solicite al usuario tres números
+# y determine cuál es el mayor.
+
+# 3- Escribe un programa que solicite al usuario una calificación
+# y determine si ha aprobado (calificación mayor o igual a 6)
+#o reprobado.
+
+# 4- Escribe un programa que solicite al usuario su edad y
+# clasifique a la persona en una categoría: niño (0-12),
+# adolescente (13-17), adulto (18-64) o adulto mayor (65+).
+
+# 5- Escribe un programa que solicite al usuario un año y
+#determine si es un año bisiesto.
+
+# 6- Escribe un programa que determine si una letra 
+# ingresada es vocal o consonante
+
+# 7- Diseña un programa que determine si 
+# un año es siglo xxi o no. Un año pertenece al 
+# sigo xxi si es mayor o igual que 2001 y/ o menor
+# o igual que 2100
+
+
+
+
+
+
+
