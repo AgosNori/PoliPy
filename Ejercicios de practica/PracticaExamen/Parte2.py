6d32be4b44c130da16744dc807acea3bdb56d976
agos <agosnoriega2018@gmail.com>
2024-06-17 13:29:09 -0300

    ejercicios bucles

---
+++ Ejercicios de practica/PracticaExamen/Parte2.py
@@ -0,0 +1,29 @@
+# Ejercicios Bucle
+# 1. Imprimir los números del 1 al 10
+
+# 2. Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces
+
+# 3. Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad)
+
+# 4. Escribir un programa que pida al usuario un numero entero positivo y muestre por pantalla la cuenta atras desde ese numero hasta cero
+
+# 5. Escribir un programa que pregunte al usuario una cantidad a invertir, el interes anual y el numero de años, y muestre por pantalla el capital obtenido en la inversion cada año que dura la inversion
+
+# 6. Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10
+
+# 7. Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre por pantalla el numero de veces que aparece la letra en la fase
+
+# 8. Escribe un programa para calcular la suma de todos los numeros pares del 1 al 100
+
+# Ahora vamos a aprender algo nuevooo .
+
+'''
+import random  se utiliza para importar el modulo "random" que contiene una variedad de funciones para generar numeros aleatorios y realizar operaciones relacionadas con la aleatoriedad. 
+Este modulo es parte de la biblioteca estandar de Py , por lo que no necesitas instalar nada adicional.
+
+
+random.randint(a,b) se utiliza para generar un numero entero aleatorio entre a y b ( ambos inclusive)'''
+
+# 9. Escribe un programa que genere un numero aleatorio entre 1 y 100 y le pida al usuario que lo adivine. El programa debe seguir pidiendo al usuario que advine hasta que acierte. Debe indicar si el numero ingresado es demasiado alto o demasiado bajo en cada intento
+
+# 10. Escribe un programa que tome una tupla de numeros. Calcule e imprima la suma de todos los numeros 
